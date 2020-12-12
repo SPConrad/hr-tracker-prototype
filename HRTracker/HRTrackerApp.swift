bc7665ba32a903b417226b3212b4d24064453aa8
Sean Conrad <spconrad@gmail.com>
2020-12-12 14:41:55 -0500

    Initial Commit

---
+++ HRTracker/HRTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRTrackerApp.swift
+//  HRTracker
+//
+//  Created by Sean Conrad on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HRTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
