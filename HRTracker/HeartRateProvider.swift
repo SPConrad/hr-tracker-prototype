920ca97cb2a5d0f7acf8479ebf716fd458056baa
Sean Conrad <spconrad@gmail.com>
2021-01-27 15:05:24 -0500

    first commit

---
+++ HRTracker/HeartRateProvider.swift
@@ -0,0 +1,18 @@
+//
+//  Cruncher.swift
+//  HRTracker
+//
+//  Created by Sean Conrad on 1/26/21.
+//
+
+import Foundation
+class HeartRateProvider {
+    /// Input: Raw HR data
+    /// Output: HR number
+    func heartRateCalculator(/* input: ambiguousHeartRateDataThing */) -> Int {
+        
+        
+        
+        return 0
+    }
+}
---
+++ HRTracker/Models/HeartRate.swift
@@ -0,0 +1,8 @@
+//
+//  HeartRate.swift
+//  HRTracker
+//
+//  Created by Sean Conrad on 1/25/21.
+//
+
+import Foundation
